effc1b636506e91a0e44891034aaa28251826d0e
DJJJJJJ <39178069+DJJJJJJ@users.noreply.github.com>
2019-01-08 23:00:24 +0800

    Update zhihu_crawler.py

--- zhihu_crawler.py
+++ zhihu_crawler.py
@@ -1,4 +1,3 @@
-# 
 import requests,pdfkit,os,re
 from bs4 import BeautifulSoup
 import smtplib
@@ -7,7 +6,7 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from email import encoders
 
-def save_as_pdf(url,name):#将爬取的文章储存为pdf
+def save_as_pdf(url,name):
     os.chdir(r'E:\DJ\py_project\crawler\zhihu')
     file_name=str(name)+'.pdf'
     if os.path.exists(file_name) or os.path.exists(file_name.replace('XnewX','')):
@@ -21,16 +20,19 @@ def save_as_pdf(url,name):#将爬取的文章储存为pdf
             print(e)
             print('failed')
 
-def rename(name):#使用正则去除命名文件名时非法的字符
+def rename(name):
     rstr = r'[\/\\\:\*\?\<\>\|]'
     new_name = re.sub(rstr, "", name)
     new_name=new_name.replace('·','')
     return new_name
 
-def send_email():#将储存在本地的pdf通过qq邮箱发送到我的kindle
+def send_email():
     path = r'E:\DJ\py_project\crawler\zhihu'
+    name_list=[]
     for file in os.listdir(path):
         if 'XnewX' in file:
+            name_list.append(file)
+            time.sleep(10)
             file_path = os.path.join(path,file)
             msg = MIMEMultipart()
             msg['Subject'] = 'convert'  #邮件标题
@@ -47,16 +49,17 @@ def send_email():#将储存在本地的pdf通过qq邮箱发送到我的kindle
             s = smtplib.SMTP_SSL("smtp.qq.com", 465,timeout = 30)#连接smtp邮件服务器,qq邮箱端口为465
             s.login(_user, _pwd)#登陆服务器
             s.sendmail(_user, _to, msg.as_string())#发送邮件
-            try:
-                os.rename(basename,basename.replace('XnewX',''))
-            except Exception as e:
-                print(e)
             s.close()
+    for name in name_list:
+        try:
+            os.rename(name, name.replace('XnewX', ''))
+        except Exception as e:
+            print(e)
 
 if __name__ == '__main__':
-    _user = "xxxxxxxx@qq.com"
-    _pwd = "*********"
-    _to = "********@kindle.cn"
+    _user = "1521804976@qq.com"
+    _pwd = "mabieleccfkyhaca"
+    _to = "8615211177226@kindle.cn"
     header = {
          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36'}
     url='http://daily.zhihu.com/'
@@ -64,8 +67,8 @@ if __name__ == '__main__':
     html=BeautifulSoup(response.text,'lxml')
     contents=html.find_all('a',attrs={'class':'link-button'})
     for content in contents:
-        new_url=url.replace('/','')+content['href'] #知乎头条文章
-        name=content.select('span')[0].text+'XnewX' #标题加上XnewX标记，表示为推送过
+        new_url=url.replace('/','')+content['href']
+        name=content.select('span')[0].text+'XnewX'
         name=rename(name)
         save_as_pdf(new_url,name)
     send_email()
